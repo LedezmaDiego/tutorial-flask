0a39979b23101451149aa5b967bc55201e8e6e8a
Diego Ajata Ledezma <ajataledezmadiego@gmail.com>
2025-03-26 11:39:06 -0300

    Agregamos tres rutas, 'chau', 'hola', y la principal, '/'

--- app.py
+++ app.py
@@ -3,5 +3,17 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def principal():
+    return """
+    <a href="/hola">Hola</a>
+    <a href="/chau">Chau</a>
+    <button>waos</button>
+"""
+
+@app.route("/hola")
+def saludar():
+    return "<h2>Hola</h2>"
+
+@app.route("/chau")
+def despedida():
+    return "<h2>Chau</h2>"
